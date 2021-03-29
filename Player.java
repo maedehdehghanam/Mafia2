1822a727080abe5566b56736af04aa8608178f8e
Maedeh <maedehdehghan@gmail.com>
2021-03-29 17:05:59 +0430

    Creating Player class

---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player
+{
+	private String name;
+	public boolean state=true;
+	private boolean mafia;
+	public Player(String name,boolean mafia)
+	{
+		this.name=name;
+		this.mafia=mafia;
+	}
+}
